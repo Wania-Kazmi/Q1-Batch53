b6add50ab6376eb4d870a21be6a1f98cceee1173
Wania-Kazmi <waniakz019@gmail.com>
2023-11-13 01:13:02 +0500

    class 2

---
+++ class02/index.js
@@ -0,0 +1,17 @@
+// console.log("Hello class 1")
+// let a = "Wania"  //type interfance 
+// let b : string = "TS 011" //explicity define type
+// console.log(a)
+// console.log(b)
+// const c = null //type interfance
+// console.log(c)
+// let d = undefined
+// console.log(d)
+function msg(strg) {
+    console.log(strg);
+}
+msg("PIAIC");
+msg("Air Uni");
+msg("Web3");
+msg(567 + 899);
+export {};
