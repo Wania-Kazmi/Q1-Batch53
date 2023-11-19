e72065877a2f0b6e24dff99f0adf36f66a209ee4
Wania-Kazmi <waniakz019@gmail.com>
2023-11-19 19:56:04 +0500

    operators, typeof

---
+++ class03/index.ts
@@ -0,0 +1,50 @@
+// let variable1 = "welcome"
+// let user = "wania"
+// console.log("Full Name is: ",variable1 + " "+ user)
+// //Template literals
+// console.log(`Full Name is: ${variable1} ${user}`)
+
+
+// let testVariable = " "; 
+// console.log(typeof testVariable);
+
+let a = 2
+let b = "2"
+
+// console.log(typeof a)
+// console.log(typeof b)
+
+// Arithmetic operators
+let a1 = 2 //assigning =
+let b1 = 2
+console.log(a1 + b1) //addition
+
+let a2 = "3"
+let b2 = "2"
+
+console.log(a2 + b2) //concatination
+
+let n = 1
+n += 5  // n = 1 - 5 = -4
+console.log(n)
+let n1 = 1
+n -= 5  // n = 1 - 5 = -4
+console.log(n1)
+
+let n2 = 4
+let n3 = 5
+
+console.log(n2 != n3)
+
+// logical operator
+
+// and (&&) or (||)
+let o1 = 4
+let o2 = 5
+
+let w1 = 2
+let w2 = 3
+
+console.log(o1 > o2 && w1 < w2) //false true = false
+console.log(o1 > o2 || w1 < w2) //false true = true
+console.log(!(o1 < 10)) //True = false
