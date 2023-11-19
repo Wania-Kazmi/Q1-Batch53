3569a6b518d0f65d660fdf0f84e03cbae3142061
Wania-Kazmi <waniakz019@gmail.com>
2023-11-19 19:55:19 +0500

    single line - multiple line comment

--- class02/index.ts
+++ class02/index.ts
@@ -1,9 +1,16 @@
 console.log("Hello class 1")
-let a = "Wania"  //type inferance
-let b : string = "TS 011" //explicity define type
+let a = "Wania";  //type inferance
+// a = 23 Error
+let b : string = "TS 011"; //explicity define type
 
+// firstName - camelcase - single line comment
+let first_name1
+
+/*
+multiple line comment
 console.log(a)
 console.log(b)
+*/
 
 const c:null = null //explicit typing
 console.log(c)
