64d6f7e8381e319b05e989fc7ae9fb048cd48c4a
Muzhar Hussain <61931803+muzhar01@users.noreply.github.com>
2024-01-08 18:27:20 +0500

    add class 10 (promises)

---
+++ class10/index.ts
@@ -0,0 +1,49 @@
+
+
+// A Promise in JavaScript is a way to handle asynchronous operations. It has three states:
+
+// Pending: The initial state when the Promise is created.
+// Fulfilled (or Resolved): The state when the asynchronous operation is successful.
+// Rejected: The state when an error or issue occurs during the asynchronous operation.
+
+function delay(milliseconds: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, milliseconds);
+    });
+}
+
+
+//Using `then`, `catch`, and `finally`  (Method Chaining):
+
+//   In JavaScript, method chaining is when you do a bunch of actions on an object one after another, without pausing in between. 
+//   It's like telling a story of tasks for the computer to follow, step by step.
+
+delay(1000)
+    .then(() => {
+        console.log('The delay is over');
+    })
+    .catch((error) => {
+        console.error('Something went wrong:', error);
+    })
+    .finally(() => {
+        console.log('This is always executed');
+    });
+
+
+//Using `async/await` (Exception Handling):
+
+try {
+    await delay(1000);
+    console.log('Await: The delay is over');
+} catch (error) {
+    console.error('Await: Something went wrong:', error);   /// If something goes wrong in the "try" part
+} finally {
+    console.log('Await: This is always executed');
+}
+
+// To understand better, try doing the activities in the article that shows you how.
+
+// https://blog.logrocket.com/async-await-in-typescript/
+
---
+++ class10/readme.md
@@ -0,0 +1,4 @@
+## Problem Solving and practice
+
+- [LeetCode - Two Some Problem](https://leetcode.com/problems/two-sum/)
+- [HackerRank - Solve Me First (sum of two integers)](https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true)
